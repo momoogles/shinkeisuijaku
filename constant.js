87b09d89ca4079c2a10809cd0123350adc03ad3c
momoogles <mo1419091@gmail.com>
2019-06-26 19:11:29 +0900

    新しい実装の仕方を模索

---
+++ constant.js
@@ -0,0 +1,30 @@
+export const GAME_STATE = {
+    START: "START",
+    SELECT_FIRST: "SELECT_FIRST",
+    SELECT_SECOND: "SELECT_SECOND",
+    CORRECT: "CORRECT",
+    INCORRECT: "INCORRECT",
+    CLEAR: "CLEAR"
+}
+
+export const JUDGE_STATE = {
+    CORRECT: "CORRECT",
+    INCORRECT: "INCORRECT"
+}
+
+export const GUIDE = {
+    START: "STARTをクリックしてください",
+    SELECT_FIRST: "1枚目を選んでください",
+    SELECT_SECOND: "2枚目を選んでください",
+    CORRECT: "正解！",
+    INCORRECT: "不正解！",
+    CLEAR: "クリア！RESETをクリックしてください"
+}
+
+export const SCORE = {
+    VALUE: 0
+}
+
+export const NODE = {
+    APP: document.getElementById("app")
+}
--- index.js
+++ index.js
@@ -1,157 +1,30 @@
-let A, B, C, D;
-let idarr =[];
-let imgarr = [];
-let turnarr = [];
-let imgswich1 = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-let imgswich2 = [0, 0, 0 ,0, 0, 0, 0, 0, 0];
-let imgnum = imgswich1.length;
-let cback = "images/card_back.png";
+import { GAME_STATE, JUDGE_STATE, GUIDE, SCORE, NODE } from "./constant.js"
 
-function reset() {
-    for(let group = 1; group <= 2; group++) {
-        for (let cnt =0; cnt < imgnum; cnt++) {
-            A = `${group}card${cnt}`;
-            document.getElementById(A).onclick = new Function(";");
-            document.getElementById(A).src = cback;
-            B = `${group}c${cnt}`;
-            C = `images/random/card_spade_${cnt}.png`;
-            idarr.push(B);
-            imgarr.push(C);
-        }
-    }
-    for (let cnt = 0; cnt < 2 * imgnum; cnt++) {
-        D = `turn${cnt}`;
-        turnarr.push(D);
-    }
-    shuffle(imgarr, idarr, turnarr);
-    console.log(imgarr, idarr, turnarr);
-    document.getElementById("btn-main").onclick = new Function("start();");
-    document.getElementById("btn-main").textContent = "START";
-    document.getElementById("guide").textContent = "STARTをClick!";
+const img_info = {
+    front: ["images/card_spade_0.png", "images/card_spade_1.png", "images/card_spade_2.png", "images/card_spade_3.png", "images/card_spade_4.png", "images/card_spade_5.png", "images/card_spade_6.png", "images/card_spade_7.png", "images/card_spade_8.png"],
+    back: "images/card_back.png",
 }
 
-function shuffle(I, J, K) {
-    for(var i = 2 * imgnum - 1; i > 0; i--) {
-        var r = Math.floor(Math.random() * (i + 1));
-        var tmp1 = I[i];
-        var tmp2 = J[i];
-        var tmp3 = K[i];
-        I[i] = I[r];
-        J[i] = J[r];
-        K[i] = K[r];
-        I[r] = tmp1;
-        J[r] = tmp2;
-        K[r] = tmp3;
+const makeCard = () => {
+    for(let i = 0; i < img_info.front.length * 2; i++) {
+        const img_back = document.createElement("img");
+        img_back.src = img_info.back;
+        NODE.APP.appendChild(img_back);
     }
 }
 
-function start() {
-    for(let cnt = 0; cnt <= imgnum - 1; cnt++) {
-        A = `1card${cnt}`;
-        let imgElement1 = document.getElementById(A)
-        imgElement1.id = idarr[cnt];
-        document.getElementById(A).onclick = new Function(`${turnarr[cnt]};`);
-        console.log(A, idarr[cnt]);
-        B = `2card${cnt}`;
-        let imgElement2 = document.getElementById(B)
-        imgElement2.id = idarr[cnt + imgnum];
-        document.getElementById(B).onclick = new Function(`${turnarr[cnt +imgnum]};`);
-        console.log(B, idarr[cnt + imgnum]);
-    }
-    document.getElementById("btn-main").onclick = new Function("reset();");
-    document.getElementById("btn-main").textContent = "RESET";
-    document.getElementById("guide").textContent = "一枚目をClick!";
-}
-
-let cfront, chid1, chid2, doubletouch, judgenum1, judgenum2, f1, f2;
-let check = 0;
-
-function turn(N) {
-    console.log(N)
-    if(check == 0) {
-        doubletouch = N;
-        if(N < imgnum) {
-            judgenum1 = N;
-            cfront = imgarr[N];
-            chid1 = `1c${judgenum1}`;
-            f1 = imgswich1[judgenum1]
-            card_src = [cback, cfront];
-            if(f1 == 1) {
-                f1 = 0;
-            }
-            else {
-                f1++;
-            }
-            document.getElementById(chid1).src = card_src[f1]
-            check++;
-        } else if(N >= imgnum) {
-            judgenum1 = N - imgnum;
-            cfront = imgarr[N];
-            chid1 = `2c${judgenum1}`
-            f1 = imgswich2[judgenum1]
-            card_src = [cback, cfront];
-            if(f1 == 1) {
-                f1 = 0;
-            }
-            else {
-                f1++;
-            }
-            document.getElementById(chid1).src = card_src[f1]
-            check++;
-        }
-        document.getElementById("guide").textContent = "2枚目のカードをClick!"
-    } else if(check == 1) {
-        console.log(doubletouch, N)
-        if(N == doubletouch) {
-            ;
-        } else {
-            if(N < imgnum) {
-                judgenum2 = N;
-                cfront = imgarr[N];
-                chid2 = `1c${judgenum2}`
-                f2 = imgswich1[judgenum2]
-                card_src = [cback, cfront];
-                if(f2 == 1) {
-                    f2 = 0;
-                }
-                else {
-                    f2++;
-                }
-                document.getElementById(chid2).src = card_src[f2]
-                check++;
-            } else if(N >= imgnum) {
-                judgenum2 = N - imgnum;
-                cfront = imgarr[N];
-                chid2 = `2c${judgenum2}`
-                f2 = imgswich2[judgenum2]
-                card_src = [cback, cfront];
-                if(f2 == 1) {
-                    f2 = 0;
-                }
-                else {
-                    f2++;
-                }
-                document.getElementById(chid2).src = card_src[f2]
-                check++;
-            }
-            if(judgenum1 == judgenum2) {
-                document.getElementById("guide").textContent = "さすが！"
-            } else {
-                document.getElementById("guide").textContent = "残念無念再来年！"
-            }
-        }
-    } else if(check == 2) {
-        console.log(judgenum1, judgenum2);
-        if(judgenum1 == judgenum2) {
-            document.getElementById(chid1).onclick = new Function(";");
-            document.getElementById(chid2).onclick = new Function(";");
-        } else {
-            f1 = 0;
-            f2 = 0;
-            document.getElementById(chid1).src = cback;
-            document.getElementById(chid2).src = cback;
-        }
-        check　= 0;
-        document.getElementById("guide").textContent = "1枚目のカードをClick!";
+const shuffleImage = () => {
+    for(let i in img_info.front) {
+        let r = Math.floor(Math.random() * (i + 1));
+        let tmp = img_info.img_array[i];
+        img_info.img_array[i] = img_info.img_array[r];
+        img_info.img_array[r] = tmp;
     }
 }
+
+const resetSection = () => {
+    makeCard();
+    shuffleImage();
+}
+
+window.onload = resetSection();
