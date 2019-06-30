092d5eaf27ca254e999b34cf33454d52e6823fb0
momoogles <mo1419091@gmail.com>
2019-06-30 20:55:49 +0900

    不正解だったらカードを閉じる所まで実装

--- constant.js
+++ constant.js
@@ -1,30 +1,57 @@
 export const GAME_STATE = {
-    START: "START",
-    SELECT_FIRST: "SELECT_FIRST",
-    SELECT_SECOND: "SELECT_SECOND",
-    CORRECT: "CORRECT",
-    INCORRECT: "INCORRECT",
-    CLEAR: "CLEAR"
-}
+  START: "START",
+  SELECT_FIRST: "SELECT_FIRST",
+  SELECT_SECOND: "SELECT_SECOND",
+  JUDGE: "JUDGE",
+  CLEAR: "CLEAR"
+};
 
 export const JUDGE_STATE = {
-    CORRECT: "CORRECT",
-    INCORRECT: "INCORRECT"
-}
+  CORRECT: true,
+  INCORRECT: false
+};
 
-export const GUIDE = {
-    START: "STARTをクリックしてください",
-    SELECT_FIRST: "1枚目を選んでください",
-    SELECT_SECOND: "2枚目を選んでください",
-    CORRECT: "正解！",
-    INCORRECT: "不正解！",
-    CLEAR: "クリア！RESETをクリックしてください"
-}
+export const CARD_STATE = {
+  FRONT: true,
+  BACK: false
+};
+
+export const GUIDE_STATE = {
+  START: "STARTをクリックしてください",
+  SELECT_FIRST: "1枚目を選んでください",
+  SELECT_SECOND: "2枚目を選んでください",
+  CORRECT: "正解！",
+  INCORRECT: "不正解！",
+  CLEAR: "クリア！RESETをクリックしてください",
+  JUDGE: "あなたはせっかちな人かプログラムの欠陥を見つけようとしてる人ですね！"
+};
 
 export const SCORE = {
-    VALUE: 0
-}
+  VALUE: 0
+};
 
 export const NODE = {
-    APP: document.getElementById("app")
-}
+  APP: document.getElementById("app"),
+  GUIDE: document.getElementById("guide")
+};
+
+export const CARDS_NAME = [
+  "spade_0",
+  "spade_0",
+  "spade_1",
+  "spade_1",
+  "spade_2",
+  "spade_2",
+  "spade_3",
+  "spade_3",
+  "spade_4",
+  "spade_4",
+  "spade_5",
+  "spade_5",
+  "spade_6",
+  "spade_6",
+  "spade_7",
+  "spade_7",
+  "spade_8",
+  "spade_8"
+];
--- index.js
+++ index.js
@@ -1,30 +1,165 @@
-import { GAME_STATE, JUDGE_STATE, GUIDE, SCORE, NODE } from "./constant.js"
+import {
+  GAME_STATE,
+  JUDGE_STATE,
+  GUIDE_STATE,
+  SCORE,
+  NODE,
+  CARD_STATE,
+  CARDS_NAME
+} from "./constant.js";
 
-const img_info = {
-    front: ["images/card_spade_0.png", "images/card_spade_1.png", "images/card_spade_2.png", "images/card_spade_3.png", "images/card_spade_4.png", "images/card_spade_5.png", "images/card_spade_6.png", "images/card_spade_7.png", "images/card_spade_8.png"],
-    back: "images/card_back.png",
+const shuffleCard = card_array => {
+  let ary = [...card_array];
+
+  for (let i = ary.length - 1; i > 0; i--) {
+    const r = Math.floor(Math.random() * (i + 1));
+    const tmp = ary[i];
+    ary[i] = ary[r];
+    ary[r] = tmp;
+  } // Fisher-Yates shuffle
+
+  return ary;
+};
+
+const makeCard = (name, key) => {
+  const div = document.createElement("div");
+  const img_back = document.createElement("img");
+  const img_front = document.createElement("img");
+
+  img_back.setAttribute("src", "images/cards/card_back.png");
+
+  img_front.setAttribute("src", `images/cards/card_${name}.png`);
+
+  div.appendChild(img_back);
+  div.addEventListener("click", () => {
+    turnCard(key);
+  });
+
+  return {
+    div: div,
+    img_back: img_back,
+    img_front: img_front
+  };
+};
+
+const openCard = card_key => {
+  const card_info = game_info.cards_info[card_key];
+
+  card_info.element.div.replaceChild(
+    card_info.element.img_front,
+    card_info.element.img_back
+  );
+
+  card_info.state = CARD_STATE.FRONT;
+};
+
+const closeCard = (key1, key2) => {
+  return new Promise(resolve => {
+    const card_info1 = game_info.cards_info[key1];
+    const card_info2 = game_info.cards_info[key2];
+
+    setTimeout(() => {
+      card_info1.element.div.replaceChild(
+        card_info1.element.img_back,
+        card_info1.element.img_front
+      );
+      card_info2.element.div.replaceChild(
+        card_info2.element.img_back,
+        card_info2.element.img_front
+      );
+
+      card_info1.state = CARD_STATE.BACK;
+      card_info2.state = CARD_STATE.BACK;
+      resolve("成功しました");
+    }, 1000);
+  });
+};
+
+const judgeCard = (key1, key2) => {
+  console.log(game_info.cards_info[key1], game_info.cards_info[key2]);
+  if (game_info.cards_info[key1].name === game_info.cards_info[key2].name) {
+    return true;
+  }
+  return false;
+};
+//ここまではコールバック関数
+
+const cards_name = shuffleCard(CARDS_NAME);
+
+const game_info = {
+  cards_info: cards_name.map((name, key) => ({
+    element: makeCard(name, key),
+    state: CARD_STATE.BACK,
+    name: name
+  })),
+
+  judge_info: {
+    card1_key: null,
+    card2_key: null
+  },
+
+  state_info: GAME_STATE.SELECT_FIRST
+};
+console.log(game_info);
+
+for (let i = 0; i < game_info.cards_info.length; i++) {
+  NODE.APP.appendChild(game_info.cards_info[i].element.div);
 }
+//ここまでは初期処理
 
-const makeCard = () => {
-    for(let i = 0; i < img_info.front.length * 2; i++) {
-        const img_back = document.createElement("img");
-        img_back.src = img_info.back;
-        NODE.APP.appendChild(img_back);
-    }
-}
+const turnCard = card_key => {
+  switch (game_info.state_info) {
+    case "SELECT_FIRST":
+      openCard(card_key);
+      game_info.judge_info.card1_key = card_key;
 
-const shuffleImage = () => {
-    for(let i in img_info.front) {
-        let r = Math.floor(Math.random() * (i + 1));
-        let tmp = img_info.img_array[i];
-        img_info.img_array[i] = img_info.img_array[r];
-        img_info.img_array[r] = tmp;
-    }
-}
+      NODE.GUIDE.textContent = GUIDE_STATE.SELECT_SECOND;
+      game_info.state_info = GAME_STATE.SELECT_SECOND;
+      break;
 
-const resetSection = () => {
-    makeCard();
-    shuffleImage();
-}
+    case "SELECT_SECOND":
+      if (game_info.cards_info[card_key].state === CARD_STATE.BACK) {
+        openCard(card_key);
+        game_info.judge_info.card2_key = card_key;
+        game_info.state_info = GAME_STATE.JUDGE;
 
-window.onload = resetSection();
+        switch (
+          judgeCard(
+            game_info.judge_info.card1_key,
+            game_info.judge_info.card2_key
+          )
+        ) {
+          case JUDGE_STATE.CORRECT:
+            NODE.GUIDE.textContent = GUIDE_STATE.CORRECT;
+
+            /*hideCard(
+              game_info.judge_info.card1_key,
+              game_info.judge_info.card2_key
+            ).then(() => {
+              const cards_state = game_info.cards_info.map(card => {
+                return card.state;
+              });
+            break;*/
+            break;
+
+          case JUDGE_STATE.INCORRECT:
+            NODE.GUIDE.textContent = GUIDE_STATE.INCORRECT;
+
+            closeCard(
+              game_info.judge_info.card1_key,
+              game_info.judge_info.card2_key
+            ).then(() => {
+              NODE.GUIDE.textContent = GUIDE_STATE.SELECT_FIRST;
+              game_info.state_info = GAME_STATE.SELECT_FIRST;
+            });
+            break;
+        }
+      }
+      break;
+
+    case "JUDGE":
+      console.log("setTimeoutによる処理が行われるまでは次の処理は行われません");
+      NODE.GUIDE.textContent = GUIDE_STATE.JUDGE;
+      break;
+  }
+};
